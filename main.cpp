759909f1725e1c4572b274bb86b778af0eea7ab0
Luiza Silva <luiza@royalsflush.net>
2012-04-26 01:03:25 -0300

    All functions are pretty much finished

--- main.cpp
+++ main.cpp
@@ -9,7 +9,95 @@ void print(Quaternion<float>& k) {
 }
 
 int main() {
-	Quaternion<float> q;
+	printf("\nConstructors test:\n");
+	
+	Quaternion<float> u;
+	printf("Default constructor - should be 1.0, 0.0, 0.0, 0.0:\n");
+	print(u);
+	
+	Quaternion<float> l(2.0, 3.0, 2323.32 , 9.921);
+	printf("Four-argument constructor - should be 2.0, 3.0, 2323.32, 9.921:\n");
+	print(l);
+	
+	printf("Vector constructor - should be 4.5, 5.6, 2.3, 7.8\n");
+	float bigV[] = {4.5, 5.6, 2.3, 7.8};
+	Quaternion<float> t(bigV);
+	print(t);
+
+	float v[] = {4., 5.5, 21};
+	Quaternion<float> q(9.3, v);
+	printf("The scalar-vector constructor - should be 9.3, 4.0, 5.5, 21.0\n");	
 	print(q);
+	
+	Quaternion<float> k(l);
+	printf("The copy constructor - should be equal to the second quaternion\n");
+	print(k);
+	printf("\n");
+
+	printf("Assignment test:\n");
+	float anotherV[] = {1., 2.323, 932.2, 5};
+	u=t= anotherV;
+	printf("Assignment operator from a vector - should be 1.0, 2.323, 9032.32, 5.0:\n");
+	print(t);
+	printf("Assignment operator from another quaternion (by passing the reference in the assignment) - should be equal to the one above\n");
+	print(u);
+
+	u=l=q;
+	printf("Testing if the reference is passed corrected - if so, the three quaternions are equal\n");
+	print(q);
+	print(l);
+	print(u); 	
+	printf("\n");
+
+	printf("Four parameter set and reference passing - both quaternions should be 1.0, 2.0, 3.0, 4.0\n");
+	u=k.set(1.0,2.0,3.0,4.0);
+	print(k);
+	print(u);
+	printf("\n");
+
+	printf("Axis-angle four-parameters assignment - angle is pi, axis is (2.0,1.0,3.0)\n");
+	printf("So quaternion should be (0.0, 2.0, 1.0, 3.0)\n");
+	printf("The second quaternion is testing the reference passed, so it should be equal to the first\n");
+	l=q.setFromAxisAngle(acos(-1.0), 2.0, 1.0, 3.0);
+	print(q);
+	print(l);
+	printf("Axis-angle scalar-vector assignment, same example as above\n");
+	float axis[] = {2.0, 1.0, 3.0};
+	k=u.setFromAxisAngle(acos(-1.0), axis);
+	print(u);
+	print(k); 
+	printf("\n");
+
+	printf("Dot product test: first quaternion is 1.0, 4.0, 8.0, 2.0;\n");
+	printf("Second quaternion is 5.0, 1.0, 3.0, 6.0\n");
+	printf("So the dot product should be equal to 45.0\n");
+	u.set(1.0, 4.0, 8.0, 2.0);
+	k.set(5.0, 1.0, 3.0, 6.0);
+	printf("%f\n", u.dot(k));
+
+	printf("Multiplication test - (using the vectors from the example above)\n");
+	t=u*k;
+	print(t);	
+	printf("*= operator test with the same quaternions\n");
+	u*=k;
+	print(u);
+	printf("Norm operation, should be 77.6853\n");
+	printf("%f\n", u.norm());
+	printf("Normalize operation - should be -0.4505, 0.8109, 0.2703, 0.2574\n");
+	print(u.normalize()); 
+
+	printf("Getting the rotation matrix, should be:\n");
+	printf(" 0.7212  0.6704  0.1739\n");
+	printf(" 0.2064 -0.4478  0.8699\n");
+	printf(" 0.6611 -0.5915 -0.4614\n\n");
+	float* mat = u.getRotMatrix();
+
+	for (int i=0; i<4; i++) {
+		for (int j=0; j<4; j++)
+			printf("%f ", mat[i*4+j]);
+		printf("\n");
+	}
+	
+
 	return 0;
 }
--- quaternion.h
+++ quaternion.h
@@ -9,25 +9,30 @@ class Quaternion {
 	T q[4];
 	
 	public:
-		Quaternion();
-		Quaternion(T a, T vx, T vy, T vz);
-		Quaternion(T a, const T* v);
-		Quaternion(Quaternion<T>& k);
-		
-		Quaternion<T>& operator=(const T* v);
-		Quaternion<T>& operator=(const Quaternion<T>& k);
-		void setFromAxisAngle(T angle, T vx, T vy, T vz);
-		void setFromAxisAngle(T angle, const T* v);
+		Quaternion(); //ok
+		Quaternion(T a, T vx, T vy, T vz); //ok
+		Quaternion(const T* v);
+		Quaternion(T a, const T* v); //ok
+		Quaternion(const Quaternion<T>& k); //ok
+	
+		Quaternion<T>& set(T a, T vx, T vy, T vz); //ok
+		Quaternion<T>& operator=(const T* v); //ok
+		Quaternion<T>& operator=(const Quaternion<T>& k); //ok
+		Quaternion<T>& setFromAxisAngle(T angle, T vx, T vy, T vz); //ok
+		Quaternion<T>& setFromAxisAngle(T angle, const T* v); //ok
 
-		Quaternion<T>& dot(const Quaternion<T>& k);
-		Quaternion<T>& cross(const Quaternion<T>& k);
+		T dot(const Quaternion<T>& k) const; //ok
+		Quaternion<T>& cross(const Quaternion<T>& k); //to do
+		T norm() const; 
+		Quaternion<T>& normalize();
 
-		Quaternion<T>& operator*=(const Quaternion<T>& k);
-		const Quaternion<T> operator*(const Quaternion<T>& k);
+		const Quaternion<T> operator*(const Quaternion<T>& k) const; //ok
+		Quaternion<T>& operator*=(const Quaternion<T>& k); //ok
 		
-		operator T*();
+		T get(int i) const; //ok
+		operator T*(); //ok
 		T* getRotMatrix();
-		T operator[](int i);
+		T& operator[](int i); //ok
 };
 
 /* Class constructors */
@@ -44,6 +49,12 @@ Quaternion<T>::Quaternion(T a, T vx, T vy, T vz) {
 }
 
 	template<typename T>
+Quaternion<T>::Quaternion(const T* v) {
+	for (int i=0; i<4; i++)
+		q[i]=v[i];
+}
+
+	template<typename T>
 Quaternion<T>::Quaternion(T a, const T* v) {
 	q[0]=a;
 	for (int i=0; i<3; i++)
@@ -51,62 +62,114 @@ Quaternion<T>::Quaternion(T a, const T* v) {
 }
 
 	template<typename T>
-Quaternion<T>::Quaternion(Quaternion<T>& k) {
+Quaternion<T>::Quaternion(const Quaternion<T>& k) {
 	for (int i=0; i<4; i++)
-		this->q[i]=k.q[i];
+		this->q[i]=k.get(i);
 }
 
 /* Setters */
 	template<typename T>
+Quaternion<T>& Quaternion<T>::set(T a, T vx, T vy, T vz) {
+	q[0]=a;
+	q[1]=vx; q[2]=vy; q[3]=vz;
+	return *this;
+}
+
+	template<typename T>
 Quaternion<T>& Quaternion<T>::operator=(const T* v) {
 	for (int i=0; i<4; i++)
 		q[i]=v[i];
+	return *this;
 }
 
 	template<typename T>
 Quaternion<T>& Quaternion<T>::operator=(const Quaternion<T>& k) {
+	if (&k==this) return *this;
 
+	for (int i=0; i<4; i++)
+		q[i]=k.get(i);
+	return *this;
 }
 
 	template<typename T>
-void Quaternion<T>::setFromAxisAngle(T angle, T vx, T vy, T vz) {
+Quaternion<T>& Quaternion<T>::setFromAxisAngle(T angle, T vx, T vy, T vz) {
 	q[0]=cos(angle/2.0);
 	double sinA = sin(angle/2.0);
 
 	q[1]=vx*sinA; q[2]=vy*sinA; q[3]=vz*sinA;
+	return *this;
 }
 
 	template<typename T>
-void Quaternion<T>::setFromAxisAngle(T angle, const T* v) {
+Quaternion<T>& Quaternion<T>::setFromAxisAngle(T angle, const T* v) {
 	q[0]=cos(angle/2.0);
 	double sinA = sin(angle/2.0);
 
 	for (int i=0; i<3; i++)
 		q[i+1]=sinA*v[i];
+	return *this;
 }
 
 /* Operations */
 	template<typename T>
-Quaternion<T>& Quaternion<T>::dot(const Quaternion<T>& k) {
-	
+T Quaternion<T>::dot(const Quaternion<T>& k) const {
+	T res=0;
+	for (int i=0; i<4; i++)
+		res+=k.get(i)*q[i];
+	return res;	
 }
 
 	template<typename T>
 Quaternion<T>& Quaternion<T>::cross(const Quaternion<T>& k) {
+	//erm...
+}
 
+	template<typename T>
+T Quaternion<T>::norm() const {
+	T n = this->dot(*this);
+	return sqrt(n);
+}
+
+	template<typename T>
+Quaternion<T>& Quaternion<T>::normalize() {
+	T n = this->norm();
+	if (n==0) return *this;
+
+	for (int i=0; i<4; i++)
+		q[i]/=n;
+	return *this;
+}
+
+	template<typename T>
+const Quaternion<T> Quaternion<T>::operator*(const Quaternion<T>& k) const {
+	Quaternion<T> res;
+
+	res[0] = q[0]*k.get(0)-q[1]*k.get(1)-q[2]*k.get(2)
+			-q[3]*k.get(3);
+ 	res[1] = q[0]*k.get(1)+q[1]*k.get(0)+q[2]*k.get(3)
+			-q[3]*k.get(2);
+	res[2] = q[0]*k.get(2)-q[1]*k.get(3)+q[2]*k.get(0)
+			+q[3]*k.get(1);
+	res[3] = q[0]*k.get(3)+q[1]*k.get(2)-q[2]*k.get(1)
+			+q[3]*k.get(0);
+
+	return res;
 }
 
 	template<typename T>
 Quaternion<T>& Quaternion<T>::operator*=(const Quaternion<T>& k) {
+	Quaternion<T> res((*this)*k);
+	*this=res;
 
-}
-
-	template<typename T>
-const Quaternion<T> Quaternion<T>::operator*(const Quaternion<T>& k) {
-
+	return *this;
 }
 
 /* Getters */
+	template<typename T>
+T Quaternion<T>::get(int i) const {
+	return q[i];
+}
+
 	template<typename T>
 Quaternion<T>::operator T*() {
 	return q;
@@ -114,11 +177,29 @@ Quaternion<T>::operator T*() {
 
 	template<typename T>
 T* Quaternion<T>::getRotMatrix() {
+	T* v = new T[16];
+	for (int i=0; i<16; i++)
+		v[i]=0;	
 
+	v[0*4+0]=1-2*q[2]*q[2]-2*q[3]*q[3];
+	v[1*4+1]=1-2*q[1]*q[1]-2*q[3]*q[3];
+	v[2*4+2]=1-2*q[1]*q[1]-2*q[2]*q[2];
+	v[3*4+3]=1;
+
+	v[0*4+1]=2*q[1]*q[2]-2*q[0]*q[3];
+	v[1*4+0]=2*q[1]*q[2]+2*q[0]*q[3];
+	
+	v[0*4+2]=2*q[1]*q[3]+2*q[0]*q[2];
+	v[2*4+0]=2*q[1]*q[3]-2*q[0]*q[2];
+	
+	v[1*4+2]=2*q[2]*q[3]-2*q[0]*q[1];
+	v[2*4+1]=2*q[2]*q[3]+2*q[0]*q[1];
+
+	return v;	
 }
 
 	template<typename T>
-T Quaternion<T>::operator[](int i) {
+T& Quaternion<T>::operator[](int i) {
 	return q[i];
 }
 
